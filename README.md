317b9c815731d4383ffd69f1598259f7295db721
Ap6y3HuK <chernenko15@bk.ru>
2020-09-13 13:11:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# memory-leacks-vs-referenced-subject-through-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/memory-leacks-vs-referenced-subject-through-template)
---
+++ src/app/target/target.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+  selector: 'wd-target',
+  templateUrl: './target.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TargetComponent  {
+  
+}
